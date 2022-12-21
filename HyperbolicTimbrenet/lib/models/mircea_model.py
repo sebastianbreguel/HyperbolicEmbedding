8144f6ca49039be48e9d194c61d33036ea61901e
sebastianbreguel <breguelsebastian@gmail.com>
2022-12-20 23:08:24 -0300

    feat separate models and modularizate values

---
+++ HyperbolicTimbrenet/lib/models/mircea_model.py
@@ -0,0 +1,175 @@
+import tensorflow as tf
+from ..hyp_model.linear_hyp import LinearHyperbolicPlusPlus
+from ..hyp_model.manifold import Poincare
+
+
+
+
+class M_VAE(tf.keras.Model):
+    def __init__(self, latent_dim):
+        super(M_VAE, self).__init__()
+        self.latent_dim = latent_dim
+        self.inference_net = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=(128, 1024, 2)),
+                
+                tf.keras.layers.Conv2D(
+                    filters=16, kernel_size=1, strides=1, padding='same'),
+                tf.keras.layers.Conv2D(
+                    filters=16, kernel_size=3, strides=1, padding='same',activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.Conv2D(
+                    filters=16, kernel_size=3, strides=1, padding='same',activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.Permute((1,3,2)),
+
+                LinearHyperbolicPlusPlus(128, Poincare(), 1, activation=tf.keras.layers.LeakyReLU(), output_hyp=True),
+                tf.keras.layers.Dropout(0.25),
+
+                LinearHyperbolicPlusPlus(64, Poincare(), 1, activation=tf.keras.layers.LeakyReLU(), output_hyp=True,input_hyp=True),
+                tf.keras.layers.Dropout(0.25),
+                LinearHyperbolicPlusPlus(16, Poincare(), 1, activation=tf.keras.layers.LeakyReLU(),input_hyp=True),
+                
+                tf.keras.layers.Permute((1,3,2)),
+
+
+
+                
+                tf.keras.layers.Conv2D(
+                    filters=32, kernel_size=3, strides=1, padding='same',activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.Conv2D(
+                    filters=32, kernel_size=3, strides=1, padding='same',activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.AveragePooling2D(
+                    pool_size=(2, 2),strides=2,padding='same'),
+                
+                tf.keras.layers.Conv2D(
+                    filters=64, kernel_size=3, strides=1, padding='same',activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.Conv2D(
+                    filters=64, kernel_size=3, strides=1, padding='same',activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.AveragePooling2D(
+                    pool_size=(2, 2),strides=2,padding='same'),
+                
+                tf.keras.layers.Conv2D(
+                    filters=128, kernel_size=3, strides=1, padding='same',activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.Conv2D(
+                    filters=128, kernel_size=3, strides=1, padding='same',activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.AveragePooling2D(
+                    pool_size=(2, 2),strides=2,padding='same'),
+                
+                tf.keras.layers.Conv2D(
+                    filters=128, kernel_size=3, strides=1, padding='same',activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.Conv2D(
+                    filters=128, kernel_size=3, strides=1, padding='same',activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.AveragePooling2D(
+                    pool_size=(2, 2),strides=2,padding='same'),
+                
+
+                tf.keras.layers.Flatten(),
+
+                LinearHyperbolicPlusPlus(latent_dim+latent_dim, Poincare(), 1),
+
+          ]
+        )
+        
+        self.generative_net = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
+
+                LinearHyperbolicPlusPlus(8*1*128, Poincare(), 1),
+
+                tf.keras.layers.Reshape(target_shape=(8, 1, 128)),
+      
+                
+                tf.keras.layers.UpSampling2D(
+                    size = 2, interpolation='nearest'),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=128, kernel_size=(2,16), strides=1, padding="SAME", activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.LayerNormalization(
+                epsilon=0.000001),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=128, kernel_size=3, strides=1, padding="SAME", activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.LayerNormalization(
+                epsilon=0.000001),
+                
+                tf.keras.layers.UpSampling2D(
+                    size = 2, interpolation='nearest'),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=128, kernel_size=(2,16), strides=1, padding="SAME", activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.LayerNormalization(
+                epsilon=0.000001),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=128, kernel_size=3, strides=1, padding="SAME", activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.LayerNormalization(
+                epsilon=0.000001),
+                
+                tf.keras.layers.UpSampling2D(
+                    size = 2, interpolation='nearest'),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=64, kernel_size=(2,16), strides=1, padding="SAME", activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.LayerNormalization(
+                epsilon=0.000001),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=64, kernel_size=3, strides=1, padding="SAME", activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.LayerNormalization(
+                epsilon=0.000001),
+                
+                tf.keras.layers.UpSampling2D(
+                    size = 2, interpolation='nearest'),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=32, kernel_size=(2,16), strides=1, padding="SAME", activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.LayerNormalization(
+                epsilon=0.000001),
+                # linear layer
+                tf.keras.layers.Conv2DTranspose(
+                    filters=32, kernel_size=3, strides=1, padding="SAME", activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.LayerNormalization(
+                epsilon=0.000001),
+                
+                tf.keras.layers.Permute((1,3,2)),
+                LinearHyperbolicPlusPlus(64, Poincare(), 1, activation=tf.keras.layers.LeakyReLU(), output_hyp=True),
+
+                tf.keras.layers.Dropout(0.25),
+                LinearHyperbolicPlusPlus(128, Poincare(), 1, activation=tf.keras.layers.LeakyReLU(), output_hyp=True,input_hyp=True),
+
+                tf.keras.layers.Dropout(0.25),
+
+                LinearHyperbolicPlusPlus(1024, Poincare(), 1, activation=tf.keras.layers.LeakyReLU(),input_hyp=True),
+                tf.keras.layers.Permute((1,3,2)),
+
+
+                tf.keras.layers.Conv2DTranspose(
+                    filters=16, kernel_size=(2,16), strides=1, padding="SAME", activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.LayerNormalization(
+                epsilon=0.000001),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=16, kernel_size=3, strides=1, padding="SAME", activation=tf.keras.layers.LeakyReLU()),
+                tf.keras.layers.LayerNormalization(
+                epsilon=0.000001),
+                
+                tf.keras.layers.Conv2DTranspose(
+                    filters=2, kernel_size=1, strides=1, padding="SAME", activation='tanh'),
+            
+            ]
+        )
+        
+    def sample(self, eps=None, apply_tanh=True):
+        if eps is None:
+            eps = tf.random.normal(shape=(2, self.latent_dim))
+        return self.decode(eps, apply_tanh)
+
+    def encode(self, x):
+        mean, logvar = tf.split(self.inference_net(x), num_or_size_splits=2, axis=1)
+        return mean, logvar
+
+    def reparameterize(self, mean, logvar):
+        eps = tf.random.normal(shape=mean.shape)
+        return eps * tf.exp(logvar * .5) + mean
+
+    def decode(self, z, apply_tanh=False):
+        logits = self.generative_net(z)
+        if apply_tanh:
+            probs = tf.math.multiply(tf.math.tanh(logits),0.999999)
+            return probs
+        else:
+            probs = tf.math.multiply(logits,0.999999)
+        return logits
+
+
